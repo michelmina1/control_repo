856770b7c162691f53d4ad9eda2d74b6575a0c18
michelmina1 <110988671+michelmina1@users.noreply.github.com>
2022-08-12 13:56:01 +0300

    Update site.pp

--- manifest/site.pp
+++ manifest/site.pp
@@ -1,10 +1,2 @@
 node default {
-  file {'/root/README1':
-  ensure => file,
-  content => 'This is a readme file',
-  owner => 'root',
-      }
-  file {'/root/READMEzzzz':
-  owner => 'root',
-  }
 }
